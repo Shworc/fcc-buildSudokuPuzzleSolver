08ba61b904e73d90637cf2331a6419a659373175
shworc <ojadekses@gmail.com>
2024-04-22 10:56:34 +0200

    Adding null value check methods

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -26,4 +26,16 @@ class Board:
                     board_string += middle_lines
             else:
                 board_string += lower_lines
-        return board_string    
+        return board_string
+    def find_empty_cell(self):
+        for row, contents in enumerate(self.board):
+            try:
+                col = contents.index(0)
+                return row, col
+            except ValueError:
+                pass
+        return None
+
+    def valid_in_row(self, row, num):
+        return num not in self.board[row]
+        
