79781d91f892902af3c1c21631f94213eca04ba9
shworc <ojadekses@gmail.com>
2024-04-24 15:21:19 +0200

    added methods valid col, valid square, is valid and solver

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -39,4 +39,34 @@ class Board:
 
     def valid_in_row(self, row, num): # checks if the number is not already present in that row
         return num not in self.board[row]
-        
+
+    def valid_in_col(self, col, num): # check whether the value at the current position in the 2D list is not equal to the provided num
+        return all(
+            self.board[row][col] != num
+            for row in range(9)
+        )
+
+    def valid_in_square(self, row, col, num): # method that checks if a number can be inserted in the 3x3 square.    
+        row_start = (row // 3) * 3 # ensure that the starting row index for each 3x3 block is a multiple of 3 
+        col_start = (col // 3) * 3 # ensure that the starting row index for each 3x3 block is a multiple of 3
+        for row_no in range(row_start, row_start + 3):
+            for col_no in range(col_start, col_start + 3): # iterate over a sequence of three elements
+                if self.board[row_no][col_no] == num:
+                    return False
+        return True
+
+    def is_valid(self, empty, num): # This method checks if a given number is a valid choice for an empty cell in the sudoku board by validating its compatibility with the row, column, and 3x3 square of the specified empty cell.
+        row, col = empty
+        valid_in_row = self.valid_in_row(row, num) # Check if the number is valid for insertion in the specified row and column, square
+        valid_in_col = self.valid_in_col(col, num)
+        valid_in_square = self.valid_in_square(row, col, num)
+        return all([valid_in_row, valid_in_col, valid_in_square])
+    
+    def solver(self): # method that attempts to solve the sudoku in-place, meaning it would modify the existing sudoku board rather than creating a new one.
+        if (next_empty := self.find_empty_cell()) is None: # (:=) By using the walrus operator, you can combine the assignment and the conditional check into a single line
+            return True
+        else:
+            for guess in range(1, 10):
+                if self.is_valid(next_empty, guess):
+                    row, col = next_empty # this is called unpack tuple :/
+
